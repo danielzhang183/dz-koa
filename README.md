2dda0c6baf108b0c0be7913926a5bd8deeb7626b
danielzhang183 <danielzhang182@gmail.com>
2022-07-03 10:03:10 +0800

    chore: add basic router

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dz-koa
+
 Learning KOA
+
+## Koa VS express
+
+<https://github.com/koajs/koa/blob/master/docs/koa-vs-express.md>
+
+## Document
+
+<https://www.yuque.com/books/share/78a0f8da-df9c-4dfc-991a-b52d07b3c3e5>
--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const Koa = require('koa')
 const app = new Koa()
 
 app.use(ctx => {
-  ctx.body = 'Hello Koa'
+  const path = ctx.path
+  switch (path) {
+    case '/':
+      ctx.body = 'home page'
+      break
+    case '/foo':
+      ctx.body = 'foo page'
+      break
+    default:
+      ctx.body = '404 Not Found.'
+  }
+  // ctx.body = 'Hello Koa'
 })
 
 app.listen(3000, () => {
